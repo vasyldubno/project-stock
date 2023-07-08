c7f390159512ab32850ea3b5a61a0b3b253705d5
Vasyl Dubno <vasyldubno@gmail.com>
2023-07-08 13:48:25 -0700

    fix

--- src/pages/api/add.ts
+++ src/pages/api/add.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { clientMongo } from "@/config/clientMongo";
 import { StockModel } from "@/models/stock-model";
+import mongoose from "mongoose";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -10,12 +11,23 @@ export default async function handler(
   // const connectedToDB = await clientMongo();
   // const r = await StockModel.findOne({ ticker: "AAPL" });
 
-  console.log(process.env.NEXT_PUBLIC_MONGODB_URL);
-  console.log(process.env.NEXT_PUBLIC_MONGODB_DATABASE);
+  try {
+    const response = await mongoose.connect(
+      process.env.NEXT_PUBLIC_MONGODB_URL as string,
+      {
+        dbName: process.env.NEXT_PUBLIC_MONGODB_DATABASE as string,
+      }
+    );
 
-  res.status(200).json({
-    message: "Ok",
-    a: process.env.NEXT_PUBLIC_MONGODB_URL,
-    b: process.env.NEXT_PUBLIC_MONGODB_DATABASE,
-  });
+    res.status(200).json({
+      message: "Ok",
+      a: process.env.NEXT_PUBLIC_MONGODB_URL,
+      b: process.env.NEXT_PUBLIC_MONGODB_DATABASE,
+      response,
+    });
+  } catch (error) {
+    res.status(200).json({
+      error,
+    });
+  }
 }
