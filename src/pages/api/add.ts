b54d97f1f33a7e34dedcd28455dcbe2e089058d9
Vasyl Dubno <vasyldubno@gmail.com>
2023-07-08 14:07:16 -0700

    fix

--- src/pages/api/add.ts
+++ src/pages/api/add.ts
@@ -1,6 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import clientMongo from "@/config/clientMongo";
 import { StockModel } from "@/models/stock-model";
+import { getXataClient } from "@/types/xata";
 import mongoose from "mongoose";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -11,10 +12,13 @@ export default async function handler(
   // const connectedToDB = await clientMongo();
   // const r = await StockModel.findOne({ ticker: "AAPL" });
 
-  const mongo = await clientMongo;
+  // const mongo = await clientMongo;
+
+  const xata = getXataClient();
+  const a = await xata.db.stock.getAll();
 
   res.status(200).json({
     message: "Ok",
-    mongo,
+    a,
   });
 }
