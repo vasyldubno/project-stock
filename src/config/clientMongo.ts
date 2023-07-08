8efa4e7bfba90f5ca1a57face7083c54eb97b614
Vasyl Dubno <vasyldubno@gmail.com>
2023-07-08 14:02:47 -0700

    fix

--- src/config/clientMongo.ts
+++ src/config/clientMongo.ts
@@ -1,28 +1,29 @@
 import mongoose from "mongoose";
 import { MongoClient } from "mongodb";
+import { MONGO_URL } from "./consts";
 
-if (!process.env.NEXT_PUBLIC_MONGODB_URL) {
+if (!MONGO_URL) {
   throw new Error('Invalid environment variable: "MONGODB_URI"');
 }
 
-const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
+const uri = MONGO_URL;
 const options = {};
 
 let client;
 let clientPromise: Promise<MongoClient>;
 
-if (!process.env.NEXT_PUBLIC_MONGODB_UR) {
+if (MONGO_URL) {
   throw new Error("Please add your Mongo URI to .env.local");
 }
 
 if (process.env.NODE_ENV === "development") {
   // In development mode, use a global variable so that the value
   // is preserved across module reloads caused by HMR (Hot Module Replacement).
-  if (!global._mongoClientPromise) {
+  if (!(global as any)._mongoClientPromise) {
     client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect();
+    (global as any)._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise;
+  clientPromise = (global as any)._mongoClientPromise;
 } else {
   // In production mode, it's best to not use a global variable.
   client = new MongoClient(uri, options);
---
+++ src/config/consts.ts
@@ -0,0 +1,3 @@
+export const MONGO_URL = process.env.NEXT_PUBLIC_MONGODB_URL as string;
+export const MONGO_DATABASE = process.env
+  .NEXT_PUBLIC_MONGODB_DATABASE as string;
