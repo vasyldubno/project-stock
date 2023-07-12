7ada6ec79538dca58127ef1879bd19f3f82b2712
Vasyl Dubno <vasyldubno@gmail.com>
2023-07-12 09:35:23 -0700

    fix

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -22,15 +22,5 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const price = await axios.get<{
-    data: { primaryData: { lastSalePrice: string } };
-  }>(`https://api.nasdaq.com/api/quote/USB/info?assetclass=stocks`);
-
-  if (price.data) {
-    const marketPrice = Number(
-      Number(price.data.data.primaryData.lastSalePrice.split("$")[1]).toFixed(2)
-    );
-  }
-
   res.status(200).json({ message: "Ok" });
 }
