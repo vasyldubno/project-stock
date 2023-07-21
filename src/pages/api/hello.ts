9ac57a9c89622966dfdf8a37a1fb0d2ebc1cbe4b
Vasyl Dubno <vasyldubno@gmail.com>
2023-07-21 10:47:33 -0700

    fix

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -10,16 +10,19 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+  const ticker = "A";
+  const exchange = "NYSE";
+
   const {
     data: { last_price },
   } = await axios.get(
-    "https://markets.sh/api/v1/symbols/NYSE:SQ?api_token=7ea62693bd4ebc0ae34595335732676b"
+    `https://markets.sh/api/v1/symbols/${exchange}:${ticker}?api_token=7ea62693bd4ebc0ae34595335732676b`
   );
 
   const stock = await supabaseClient
     .from("stock")
     .select()
-    .eq("ticker", "A")
+    .eq("ticker", ticker)
     .single();
 
   if (stock.data && last_price) {
@@ -27,7 +30,7 @@ export default async function handler(
     await supabaseClient
       .from("stock")
       .update({ price_current: price })
-      .eq("ticker", "A");
+      .eq("ticker", ticker);
   }
 
   // await supabaseClient.from("test").insert({ content: last_price });
