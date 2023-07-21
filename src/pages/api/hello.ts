ebefc006c1399695ae00c3239b0c10a41733c44a
Vasyl Dubno <vasyldubno@gmail.com>
2023-07-21 10:55:11 -0700

    fix

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -10,30 +10,34 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const ticker = "A";
+  const tickers = ["A", "AAP"];
   const exchange = "NYSE";
 
-  const {
-    data: { last_price },
-  } = await axios.get(
-    `https://markets.sh/api/v1/symbols/${exchange}:${ticker}?api_token=7ea62693bd4ebc0ae34595335732676b`
-  );
+  tickers.forEach((stock, index) => {
+    setTimeout(async () => {
+      const {
+        data: { last_price },
+      } = await axios.get(
+        `https://markets.sh/api/v1/symbols/${exchange}:${stock}?api_token=7ea62693bd4ebc0ae34595335732676b`
+      );
 
-  const stock = await supabaseClient
-    .from("stock")
-    .select()
-    .eq("ticker", ticker)
-    .single();
+      const stockSupa = await supabaseClient
+        .from("stock")
+        .select()
+        .eq("ticker", stock)
+        .single();
 
-  if (stock.data && last_price) {
-    const price = Number(last_price.toFixed(2));
-    await supabaseClient
-      .from("stock")
-      .update({ price_current: price })
-      .eq("ticker", ticker);
-  }
+      if (stockSupa.data && last_price) {
+        const price = Number(last_price.toFixed(2));
+        await supabaseClient
+          .from("stock")
+          .update({ price_current: price })
+          .eq("ticker", stock);
+      }
 
-  // await supabaseClient.from("test").insert({ content: last_price });
+      // await supabaseClient.from("test").insert({ content: last_price });
+    }, index * 1000);
+  });
 
   res.json({
     message: "Ok",
